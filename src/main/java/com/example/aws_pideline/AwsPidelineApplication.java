f3751f431129d7f3013a882cd7863a07f1e2327b
Maharana <45598520+Maharana-Pratap@users.noreply.github.com>
2025-08-15 12:55:59 +0530

    first commit

---
+++ src/main/java/com/example/aws_pideline/AwsPidelineApplication.java
@@ -0,0 +1,13 @@
+package com.example.aws_pideline;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwsPidelineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AwsPidelineApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/aws_pideline/AwsPidelineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.aws_pideline;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AwsPidelineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
